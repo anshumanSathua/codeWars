3cba9ba7fd377f8e81b51d461b6b48ac1d711b8d
Anshuman Sathua <95961359+anshu-21@users.noreply.github.com>
2022-06-04 21:55:50 +0530

    Update 06-04-22-052.js

--- programs/06-04-22-052.js
+++ programs/06-04-22-052.js
@@ -12,10 +12,10 @@ function isValidWalk(walk) {
     ew = 0;
   if (walk.length === 10) {
     for (let i of walk) {
-      if (i == "n") ns += 1;
-      if (i == "s") ns -= 1;
-      if (i == "e") ew += 1;
-      if (i == "w") ew -= 1;
+      if (i == "n") ns ++;
+      if (i == "s") ns --;
+      if (i == "e") ew ++;
+      if (i == "w") ew --;
     }
   } else return false;
   return ns === 0 && ew === 0;
