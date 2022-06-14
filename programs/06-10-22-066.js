ce25db2bec0f9211272faf974bcdddd672b1d589
Anshuman Sathua <co.anshu@gmail.com>
2022-06-14 16:13:47 +0530

    first commit

---
+++ programs/06-10-22-066.js
@@ -0,0 +1,63 @@
+/******************
+ * 
+ * You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+For example, when the input is green, output should be yellow.
+ * 
+ */
+
+//S1
+function updateLight(current) {
+  const arr = ["green", "yellow", "red"];
+  current = current.toLowerCase();
+
+  if (arr.includes(current) && arr.indexOf(current) === arr.length - 1) {
+    return arr[0];
+  } else {
+    return arr[arr.indexOf(current) + 1];
+  }
+}
+
+//S2
+function updateLight(current) {
+  return current === "yellow"
+    ? "red"
+    : current === "green"
+    ? "yellow"
+    : "green";
+}
+
+//S3
+const updateLight = (current) =>
+  ({
+    green: "yellow",
+    yellow: "red",
+    red: "green",
+  }[current]);
+
+//S4
+function updateLight(current) {
+  switch (current) {
+    case "green":
+      return "yellow";
+      break;
+    case "yellow":
+      return "red";
+      break;
+    case "red":
+      return "green";
+      break;
+    default:
+      throw "Error: wrong input";
+      break;
+  }
+}
+
+//S5
+function updateLight(current) {
+  const states = ["green", "yellow", "red"];
+  let nextStateIndex = (states.indexOf(current) + 1) % states.length;
+  return states[nextStateIndex];
+}
