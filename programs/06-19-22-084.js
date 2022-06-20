794a0a725b3da1777ee348793c7545c417c7a1f4
Anshuman Sathua <co.anshu@gmail.com>
2022-06-20 23:35:37 +0530

    06-19-22-commit

---
+++ programs/06-19-22-084.js
@@ -0,0 +1,14 @@
+/*************
+ * 
+ * Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+Note: input will never be an empty string
+ * 
+ */
+
+//S1
+const fakeBin = (x) =>
+  x
+    .split("")
+    .map((a) => (+a < 5 ? 0 : 1))
+    .join("");
