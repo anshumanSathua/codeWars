3493d49f9e37da629d80cba504416f2aa104c1c5
Anshuman Sathua <co.anshu@gmail.com>
2022-07-08 12:50:41 +0530

    07-08-22-commit

--- programs/07-08-22-126.js
+++ programs/07-08-22-126.js
@@ -11,3 +11,16 @@ const find_average = (array) => {
   let sum = array.reduce((a, b) => a + b, 0);
   return array.length !== 0 ? sum / array.length : 0;
 };
+
+//S2
+const findAvg = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  if (arr.length === 0) {
+    return 0;
+  } else {
+    return sum / arr.length;
+  }
+};
