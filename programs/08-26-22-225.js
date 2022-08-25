e2788651b56e632a4dfba871138b43eab833812f
Anshuman Sathua <co.anshu@gmail.com>
2022-08-26 01:59:28 +0530

    08-26-22-commit

---
+++ programs/08-26-22-225.js
@@ -0,0 +1,10 @@
+/*****************
+ *
+ * Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+ *
+ */
+
+//S1
+const include = (arr, item) => {
+  return arr.includes(item);
+};
