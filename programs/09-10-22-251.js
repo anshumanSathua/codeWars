1d9937e31382c23036d0770d4fe839595596808a
Anshuman Sathua <co.anshu@gmail.com>
2022-09-10 13:35:17 +0530

    09-10-22-commit

---
+++ programs/09-10-22-251.js
@@ -0,0 +1,28 @@
+/****************************
+ *
+ *We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
+
+So given a string "super", we should return a list of [2, 4].
+
+Some examples:
+Mmmm  => []
+Super => [2,4]
+Apple => [1,5]
+YoMama -> [1,2,4,6]
+NOTES
+Vowels in this context refers to: a e i o u y (including upper case)
+This is indexed from [1..n] (not zero indexed!)
+ *
+ */
+
+//S1
+const vowelIndices = (w) => {
+  let idx = [];
+  let v = ["a", "e", "i", "o", "u", "y"];
+  w.toLowerCase()
+    .split("")
+    .map((el, i) => {
+      if (v.includes(el)) idx.push(i + 1);
+    });
+  return idx;
+};
