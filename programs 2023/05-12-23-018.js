54b6d921d13949ac59be8e589851ac5ad29962ee
Anshuman Sathua <co.anshu@gmail.com>
2023-05-12 22:01:30 +0530

    05-12-23-018.js added

---
+++ programs 2023/05-12-23-018.js
@@ -0,0 +1,77 @@
+/**********************
+ *
+ *Given an array of integers of any length, return an array that has 1 added to the value represented by the array.
+
+the array can't be empty
+only non-negative, single digit integers are allowed
+Return nil (or your language's equivalent) for invalid inputs.
+
+Examples
+Valid arrays
+
+[4, 3, 2, 5] would return [4, 3, 2, 6]
+[1, 2, 3, 9] would return [1, 2, 4, 0]
+[9, 9, 9, 9] would return [1, 0, 0, 0, 0]
+[0, 1, 3, 7] would return [0, 1, 3, 8]
+
+Invalid arrays
+
+[1, -9] is invalid because -9 is not a non-negative integer
+
+[1, 2, 33] is invalid because 33 is not a single-digit integer
+ */
+
+//S1
+const upArray = (arr) => {
+  if (
+    !arr ||
+    !arr.length ||
+    !arr.every((x) => Number.isInteger(x) && x >= 0 && x <= 9)
+  ) {
+    return null;
+  }
+  let x = 1;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    x += arr[i];
+    if (x < 10) {
+      arr[i] = x;
+      x = 0;
+      break;
+    }
+    arr[i] = 0;
+    x = 1;
+  }
+  if (x === 1) {
+    arr.unshift(1);
+  }
+  return arr;
+};
+
+//S2
+function upArray(arr) {
+  if (typeof arr === "undefined" || arr === null || arr.length === 0) {
+    return null;
+  }
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] < 0 || typeof arr[i] !== "number" || arr[i] > 9) {
+      return null;
+    }
+  }
+  // if its 9 and more than one digit we have to check all previous digits
+  // whether they are also a 9
+  for (var j = arr.length - 1; j > -1; j--) {
+    if (arr[j] !== 9) {
+      arr[j] = arr[j] + 1;
+      break;
+    } else {
+      arr[j] = 0;
+    }
+
+    if (j === 0) {
+      arr.unshift(1);
+    }
+  }
+
+  return arr;
+}
