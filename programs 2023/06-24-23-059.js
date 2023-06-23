cfcfe947ada133ac9ba0472aa209f3e0a9aa298f
Anshuman Sathua <co.anshu@gmail.com>
2023-06-24 01:41:22 +0530

    06-24-23-059.js added

---
+++ programs 2023/06-24-23-059.js
@@ -0,0 +1,75 @@
+/****************************
+ * Introduction and Warm-up (Highly recommended)
+Playing With Lists/Arrays Series
+Task
+Given an array/list [] of integers , Construct a product array Of same size Such That prod[i] is equal to The Product of all the elements of Arr[] except Arr[i].
+
+Notes
+Array/list size is at least 2 .
+
+Array/list's numbers Will be only Positives
+
+Repetition of numbers in the array/list could occur.
+
+Input >> Output Examples
+productArray ({12,20}) ==>  return {20,12}
+Explanation:
+The first element in prod [] array 20 is the product of all array's elements except the first element
+
+The second element 12 is the product of all array's elements except the second element .
+
+productArray ({1,5,2}) ==> return {10,2,5}
+Explanation:
+The first element 10 is the product of all array's elements except the first element 1
+
+The second element 2 is the product of all array's elements except the second element 5
+
+The Third element 5 is the product of all array's elements except the Third element 2.
+
+productArray ({10,3,5,6,2}) return ==> {180,600,360,300,900}
+Explanation:
+The first element 180 is the product of all array's elements except the first element 10
+
+The second element 600 is the product of all array's elements except the second element 3
+
+The Third element 360 is the product of all array's elements except the third element 5
+
+The Fourth element 300 is the product of all array's elements except the fourth element 6
+
+Finally ,The Fifth element 900 is the product of all array's elements except the fifth element 2
+
+A more challenging version of this kata by Firefly2002
+Playing with Numbers Series
+Playing With Lists/Arrays Series
+For More Enjoyable Katas
+ALL translations are welcomed
+Enjoy Learning !!
+Zizou
+ * 
+ */
+
+//S1
+function productArray(numbers) {
+  const n = numbers.length;
+  const product = Array(n).fill(1);
+
+  let leftProduct = 1;
+  for (let i = 0; i < n; i++) {
+    product[i] *= leftProduct;
+    leftProduct *= numbers[i];
+  }
+
+  let rightProduct = 1;
+  for (let i = n - 1; i >= 0; i--) {
+    product[i] *= rightProduct;
+    rightProduct *= numbers[i];
+  }
+
+  return product;
+}
+
+//S2
+function productArray(numbers) {
+  const product = numbers.reduce((acc, curr) => acc * curr, 1);
+  return numbers.map((num) => product / num);
+}
