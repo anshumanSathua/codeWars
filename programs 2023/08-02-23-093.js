e665dd17ff3fc6dbbf8768413ee9eb2591bfc761
Anshuman Sathua <co.anshu@gmail.com>
2023-08-02 00:56:14 +0530

    08-02-23-093.js added

---
+++ programs 2023/08-02-23-093.js
@@ -0,0 +1,10 @@
+/*****************
+ *Create a function called args_count that returns the number of arguments provided
+ * args_count(1, 2, 3) -> 3
+args_count(1, 2, 3, 10) -> 4
+ */
+
+//S1
+const args_count = (...args) => {
+  return args.length;
+};
