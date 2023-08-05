4e8d9e9097b3f732d053507ed80dec222d536541
Anshuman Sathua <co.anshu@gmail.com>
2023-08-05 22:47:46 +0530

    08-06-23-097.js added

---
+++ programs 2023/08-06-23-097.js
@@ -0,0 +1,45 @@
+/*****************************
+ * Modify the kebabize function so that it converts a camel case string into a kebab case.
+
+"camelsHaveThreeHumps"  -->  "camels-have-three-humps"
+"camelsHave3Humps"  -->  "camels-have-humps"
+"CAMEL"  -->  "c-a-m-e-l"
+Notes:
+
+the returned string should only contain lowercase letters
+ * 
+ */
+
+//S1
+function kebabize(str) {
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (char >= "A" && char <= "Z") {
+      if (i !== 0 && result[result.length - 1] !== "-") {
+        result += "-";
+      }
+      result += char.toLowerCase();
+    } else if (char >= "a" && char <= "z") {
+      result += char;
+    }
+  }
+
+  return result;
+}
+
+//S2
+function kebabize(str) {
+  return str
+    .split("")
+    .filter((v) => isNaN(parseInt(v)))
+    .map((v) =>
+      v.toUpperCase() === v
+        ? v.replace(v, "-" + v.toLowerCase())
+        : v.toLowerCase()
+    )
+    .join("")
+    .replace(/^-/, "");
+}
